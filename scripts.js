c0f5bf8ed0f9a755d7965edd410c1a0b3aa2884f
wabuko <wabuko.akhanyinya@student.moringaschool.com>
2021-07-25 19:04:08 +0300

    add final changes

--- scripts.js
+++ scripts.js
@@ -1,66 +1,66 @@
-$(document).ready(function(){
-    $('#design').click(function(){
+$(document).ready(function () {
+    $('#design').click(function () {
         $('#design').hide()
     })
-    $('#design').click(function(){
+    $('#design').click(function () {
         $('.num1').toggle()
     })
-    $('.num1').click(function(){
+    $('.num1').click(function () {
         $('.num1').hide()
         $('#design').show()
     })
 
-    $('#dev').click(function(){
+    $('#dev').click(function () {
         $('#dev').hide()
     })
-    $('#dev').click(function(){
+    $('#dev').click(function () {
         $('.num2').toggle()
     })
-    $('.num2').click(function(){
+    $('.num2').click(function () {
         $('.num2').hide()
         $('#dev').show()
     })
-    $('#product').click(function(){
+    $('#product').click(function () {
         $('#product').hide()
     })
-    $('#product').click(function(){
+    $('#product').click(function () {
         $('.num3').toggle()
     })
-    $('.num3').click(function(){
+    $('.num3').click(function () {
         $('.num3').hide()
         $('#product').show()
     })
-    $('#work4').hover(function(){
+    $('#work4').hover(function () {
         $('.work4').toggle()
     })
-    $('#work5').hover(function(){
+    $('#work5').hover(function () {
         $('.work5').toggle()
     })
-    $('#work6').hover(function(){
+    $('#work6').hover(function () {
         $('.work6').toggle()
     })
-    $('#work7').hover(function(){
+    $('#work7').hover(function () {
         $('.work7').toggle()
     })
-    $('#work8').hover(function(){
+    $('#work8').hover(function () {
         $('.work8').toggle()
     })
-    $('#work3').hover(function(){
+    $('#work3').hover(function () {
         $('.work3').toggle()
     })
-    $('#work2').hover(function(){
+    $('#work2').hover(function () {
         $('.work2').toggle()
     })
-    $('#work1').hover(function(){
+    $('#work1').hover(function () {
         $('.work1').toggle()
     })
-    
+
 })
-function message(){
+function message() {
     let name = document.getElementById('jina').value
     if (name === '') {
         alert("Please enter your name")
     } else {
-        alert(name + " "+ "we have received your message. Thank you for reaching out to us.")  
+        alert(name + " " + "we have received your message. Thank you for reaching out to us.")
     }
 }
