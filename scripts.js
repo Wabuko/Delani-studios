6a6e2feca42607c2c2f9d5766fe128d89b215ae7
wabuko <wabuko.akhanyinya@student.moringaschool.com>
2021-07-25 18:27:42 +0300

    add hover effects

--- scripts.js
+++ scripts.js
@@ -30,4 +30,37 @@ $(document).ready(function(){
         $('.num3').hide()
         $('#product').show()
     })
+    $('#work4').hover(function(){
+        $('.work4').toggle()
+    })
+    $('#work5').hover(function(){
+        $('.work5').toggle()
+    })
+    $('#work6').hover(function(){
+        $('.work6').toggle()
+    })
+    $('#work7').hover(function(){
+        $('.work7').toggle()
+    })
+    $('#work8').hover(function(){
+        $('.work8').toggle()
+    })
+    $('#work3').hover(function(){
+        $('.work3').toggle()
+    })
+    $('#work2').hover(function(){
+        $('.work2').toggle()
+    })
+    $('#work1').hover(function(){
+        $('.work1').toggle()
+    })
+    
 })
+function message(){
+    let name = document.getElementById('jina').value
+    if (name === '') {
+        alert("Please enter your name")
+    } else {
+        alert(name + " "+ "we have received your message. Thank you for reaching out to us.")  
+    }
+}
