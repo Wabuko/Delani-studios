efd8a360d03390bd883d0e6de9acfa31e517e1e0
Wabuko <87002634+Wabuko@users.noreply.github.com>
2021-07-30 12:33:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-
+$(document).ready(function () {
+    $('#design').click(function () {
+        $('#design').hide()
+    })
+    $('#design').click(function () {
+        $('.num1').toggle()
+    })
+    $('.num1').click(function () {
+        $('.num1').hide()
+        $('#design').show()
+    })
 
     $('#dev').click(function () {
         $('#dev').hide()
